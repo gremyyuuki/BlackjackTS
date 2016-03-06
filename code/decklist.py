3130e6bca80f4dadfe8a356a927230cffb71fc5b
JeremyT <gremy.moi@gmail.com>
2016-03-06 16:36:43 +0100

    affichage d'une carte sur un canvas revision du ppt qui montre l'enchainement des etapes comptage de la valeur d'une main boucle principale

--- code/decklist.py
+++ code/decklist.py
@@ -1,18 +1,12 @@
-# nombre = valeurs que peuvent prendre une carte
-nombre = [1,2,3,4,5,6,7,8,9,10,11]
+DECK = {'ascoeur':11,'2coeur':2,'3coeur':3,'4coeur':4,'5coeur':5,'6coeur':6,'7coeur':7,'8coeur':8,'9coeur':9,'10coeur':10,'valetcoeur':10,'damecoeur':10,'roicoeur':10,'ascarreau':11,'2carreau':2,'3carreau':3,'4carreau':4,'5carreau':5,'6carreau':6,'7carreau':7,'8carreau':8,'9carreau':9,'10carreau':10,'valetcarreau':10,'damecarreau':10,'roicarreau':10,'astrefle':11,'2trefle':2,'3trefle':3,'4trefle':4,'5trefle':5,'6trefle':6,'7trefle':7,'8trefle':8,'9trefle':9,'10trefle':10,'valettrefle':10,'dametrefle':10,'roitrefle':10,'aspique':11,'2pique':2,'3pique':3,'4pique':4,'5pique':5,'6pique':6,'7pique':7,'8pique':8,'9pique':9,'10pique':10,'valetpique':10,'damepique':10,'roipique':10}
 # deck = ensemble de cartes du jeu
 # _RC_ 20160214 expliquer différences entre deck et DECK
 # _RC_ 20160214 il vaut mieux changer de nom que de différencier seulement sur la casse
-DECK = ['ascoeur','2coeur','3coeur','4coeur','5coeur','6coeur','7coeur','8coeur','9coeur','10coeur','valetcoeur','damecoeur','roicoeur','ascarreau','2carreau','3carreau','4carreau','5carreau','6carreau','7carreau','8carreau','9carreau','10carreau','valetcarreau','damecarreau','roicarreau','astrefle','2trefle','3trefle','4trefle','5trefle','6trefle','7trefle','8trefle','9trefle','10trefle','valettrefle','dametrefle','roitrefle','aspique','2pique','3pique','4pique','5pique','6pique','7pique','8pique','9pique','10pique','valetpique','damepique','roipique']
 deck = list(DECK)
-#famille = ('coeur', 'trefle', 'pique', 'carreau')
-#carte = ('A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K')
-#valeur = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, 'T':10, 'J':10, 'Q':10, 'K':10}
-#from random import choice as rc
-#import sys
+
 import random
 import math
-import tkinter
+#import tkinter
 
 Victoires_joueur=0
 Victoires_croupier=0
@@ -30,45 +24,45 @@ def tirer(hand,x):
         # ne pas utiliser '=', sinon, ne modifie pas la variable passée en paramètre
         hand.append( deck.pop(0) )
 
+# _RC_ je ne comprends pas l'utilité de ça
+# y est une variable locale, donc à la sortie de 'prendre', je n'ai rien
 def prendre(x):
     y=[]
     for i in range(x):
         tirer(y,1)
     print(y)
 
-prendre(1)
-
-
-
 #Fonction qui compte la valeur totale de la main
 def total(hand):
-    nbrAS=hand.count(11) #Compte le nombre d'as dans la main
-    t=sum(hand)
+    t     = 0
+    nbrAS = 0
+    nbrCartes = len(hand)
+    for i in range(nbrCartes):
+        carte  = hand[ i ]
+        valeur = DECK.get(carte)
+        if valeur == 11:#Compte le nombre d'as dans la main
+            nbrAS += 1
+        t      = t + valeur
+    print("nb d'as = ", nbrAS)
     while nbrAS>0 and t>21:
-        t-=10
-        nbrAS-=1
+        t     -= 10
+        nbrAS -= 1
     return t
 
-def hand_du_croupier():
-    hand=[]
-    tirer(hand,1)            #Le croupier commence par tirer une carte
-    while total(hand) < 17:
-        tirer(hand,1)#Le croupier tire jusqu'a 17 et s'arrete au dessus
-    print(hand)
-    hand_croupier=total(hand)
+def croupier(hand_croupier):    
+    #Le croupier a déj une ou plusieurs cartes
+    while total(hand_croupier) < 17:
+        tirer(hand_croupier,1)#Le croupier tire jusqu'a 17 et s'arrete au dessus
+        print(hand_croupier)
+    print("Score du croupier:"+str(total(hand_croupier)))
 
-def hand_du_joueur():#finir
+def joueur(hand_joueur):
     x=input("Voulez vous une autre carte ?\nSi oui, tapez h, sinon tapez s:")
-    if x == "h":
-        tirer(hand,1)
-        total(hand)
-        if total(hand) <=21:
-            hand_du_joueur()
-        else:
-            gagnant()
-    else:
-        gagnant()
-
+    while x == "h":
+        tirer(hand_joueur,1)
+        print("Votre score:"+str(total(hand_joueur)))
+        print(hand_joueur)
+        x=input("Voulez vous une autre carte ?\nSi oui, tapez h, sinon tapez s:")
 
 #Fonction qui compare le score de chaque joueur et affiche le nombre de victoires
 def gagnant():
@@ -100,4 +94,40 @@ def gagnant():
     elif hand_croupier == hand_joueur:
         print("ÉGALITÉ")
     print(Victoire_croupier, Victoire_joueur)
+
+def comptage():
+    compte=0
+    if hand=='2coeur'or'2trefle'or'2carreau'or'2pique'or'3coeur'or'3trefle'or'3carreau'or'3pique'or'4coeur'or'4trefle'or'4carreau'or'4pique'or'5coeur'or'5trefle'or'5carreau'or'5pique'or'6coeur'or'6trefle'or'6carreau'or'6pique':
+        compte += 1
+    elif hand=='7coeur'or'7trefle'or'7carreau'or'7pique'or'8coeur'or'8trefle'or'8carreau'or'8pique'or'9coeur'or'9trefle'or'9carreau'or'9pique':
+        compte = compte
+    elif hand=='10coeur'or'10trefle'or'10carreau'or'10pique'or'valetcoeur'or'valettrefle'or'valetcarreau'or'valetpique'or'damecoeur'or'dametrefle'or'damecarreau'or'damepique'or'roicoeur'or'roitrefle'or'roicarreau'or'roipique'or'ascoeur'or'aspique'or'ascarreau'or'astrefle':
+        compte -= 1
+
+# Programme principal
+#print(deck)
+
+
+r="r"
+while r=="r":
+    #Initialiser les mains
+    hand_joueur   = []
+    hand_croupier = []
+    #Tirer une carte pour le croupier
+    tirer(hand_croupier,1)
+    print("main croupier = ", hand_croupier)
+    #Tirer deux cartes pour le joueur
+    tirer(hand_joueur,2)
+    print("main joueur = ", hand_joueur)
+    print("score joueur = ", total(hand_joueur))
+    #Le joueur joue
+    joueur(hand_joueur)
+    #Le croupier joue
+    croupier(hand_croupier)
+    #Comparaison des scores
+
+    #Question pour rejouer ou quitter
+    r=input("Voulez vous rejouer ?\nSi oui, tapez r, sinon tapez q:")
+
+print("Merci d'avoir joué.\nAu revoir")
 
--- code/interface_graphique.py
+++ code/interface_graphique.py
@@ -4,28 +4,34 @@ x=1
 y=0
 fenetre = Tk()  #fenetre principale
 
+
 champ_label = Label(fenetre, text="Jeu de Blackjack")
 champ_label.pack()
 
-cadre = Frame(fenetre, width=1500, height=1000, borderwidth=2, bg="green")#cadre ou il y aura les scores et les cartes
+cadre = Frame(fenetre, width=1, height=1, borderwidth=2, bg="#1D702D")#cadre ou il y aura les scores et les cartes
 cadre.pack(fill=BOTH)
 
-message1 = Label(cadre, text="Votre score : "+str(y), fg="white", bg="green")#Scores
+message1 = Label(cadre, text="Votre score : "+str(y), fg="white", bg="#1D702D")#Scores
 message1.pack(side="top", fill=X)
-message2 = Label(cadre, text="Score du croupier : "+str(x), fg="white", bg="green")
+message2 = Label(cadre, text="Score du croupier : "+str(x), fg="white", bg="#1D702D")
 message2.pack(side="top", fill=X)
 
-filepath = askopenfilename(title="Ouvrir une image",filetypes=[('gif files','.gif'),('all files','.*')])#affiche une carte
-photo = PhotoImage(file=filepath)                                                                   #a partir d'un dossier
-canvas = Canvas(cadre, width=photo.width(), height=photo.height(), bg="green")
-canvas.create_image(0, 0, anchor=NW, image=photo)
-canvas.pack()
+#filepath = askopenfilename(title="Ouvrir une image",filetypes=[('gif files','.gif'),('all files','.*')])#affiche une carte
+#photo = PhotoImage(file=filepath)                                                                   #a partir d'un dossier
+#canvas = Canvas(cadre, width=photo.width(), height=photo.height(), bg="#1D702D")
+#canvas.create_image(0, 0, anchor=NW, image=photo)
+#canvas.pack()
+canvas = Canvas(width = 400, height = 400, bg = "#1D702D")
+canvas.pack(fill=BOTH)
+
+gif1 = PhotoImage(file = "..\\cartes\\4_coeur.gif")
+canvas.create_image(50, 50, image = gif1, anchor = NW)
 
 bouton_rejouer = Button(cadre, text="Rejouer")
 bouton_rejouer.pack()
 
-bouton_quitter = Button(fenetre, text="Quitter", command = fenetre.quit)
-bouton_quitter.pack()
+bouton_quitter = Button(fenetre, text="Quitter", command = fenetre.destroy)
+bouton_quitter.pack(side = RIGHT, padx = 0, pady = 0)
 
 fenetre.mainloop()
-fenetre.destroy()
+
