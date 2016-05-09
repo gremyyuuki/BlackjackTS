4018e08274414645a6b3fb46e3d2a561630401f1
gremyyuuki <gremy.moi@gmail.com>
2016-05-09 15:51:04 +0200

    Update interface_graphique.py

--- code/interface_graphique.py
+++ code/interface_graphique.py
@@ -65,6 +65,15 @@ def rester():#fonction du bouton rester
         canvas.create_text(260,260, text = "PERDU" , fill="white")
     fenetre.update_idletasks()
 
+def button_aide():
+    new_window=Tk()
+    cnv = Canvas(new_window,width=900, height=400)
+    cnv.pack()
+    labl = Label(new_window,text="Aide")
+    labl.pack()
+    tableau = PhotoImage(file = "tabstrat1.gif", master= cnv)
+    cnv.create_image(1,1,image=tableau,anchor = NW)
+    new_window.mainloop()
 
 hand_croupier = []
 hand_joueur = []
@@ -106,16 +115,6 @@ bouton_rejouer.pack(side="top")
 bouton_quitter = Button(fenetre, text="Quitter", command = fenetre.destroy)#bouton quitter
 bouton_quitter.pack(side="bottom")
 
-def button_aide():
-    new_window=Tk()
-    cnv = Canvas(new_window,width=900, height=400)
-    cnv.pack()
-    labl = Label(new_window,text="Aide")
-    labl.pack()
-    tableau = PhotoImage(file = "tabstrat1.gif", master= cnv)
-    cnv.create_image(1,1,image=tableau,anchor = NW)
-    new_window.mainloop()
-    
 boutton_aide = Button(fenetre,text="Aide", command = button_aide )
 boutton_aide.pack(side="bottom")
 
